678bc2a22f69e9b0f181532b2c6b9faebdaaaa17
FrancoMonzon <francodavidmonzon@hotmail.com>
2024-06-22 21:23:57 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <cstdlib>
 #include <cstdio>
 #include <ctime>
-/// #include "rlutil.h"
+#include "rlutil.h"
 
 using namespace std;
 
@@ -12,30 +12,37 @@ int main()
 {
     int opc;
 
-        while(true){ /// al ponerlo en false el programa se corrompe y se muestra todo en negro
+    system("color 5f");
+
+    while(true)  /// al ponerlo en false el programa se corrompe y se muestra todo en negro
+    {
         system("cls"); /// al quitarlo, al finalizar una partida no se limpiaria la pantalla
 
-    /// utilizando rlutil, ubicamos el menu en la columna y fila deseada, en este caso seria en el medio de la pantalla, tambien se podria hacer lo mismo con las demas cosas
+        /// utilizando rlutil, ubicamos en la columna y fila deseada, en este caso seria en el medio de la pantalla
 
-    /// rlutil::locate(30, 7);
+        rlutil::locate(30, 7);
         cout << "#################################################" <<endl;
-    /// rlutil::locate(30, 8);
+        rlutil::locate(30, 8);
         cout << "###### TP FINAL - FUENTES, BOTTELI, MONZON ######" << endl;
-    /// rlutil::locate(30, 9);
+        rlutil::locate(30, 9);
         cout << "#################################################" << endl;
-    /// rlutil::locate(30, 10);
+        rlutil::locate(30, 10);
         cout << endl;
-    /// rlutil::locate(30, 11);
+        rlutil::locate(30, 11);
+        cout << "MENU PRINCIPAL"<<endl;
+        rlutil::locate(30, 12);
         cout << "------------------------------------------------- " << endl;
-    /// rlutil::locate(30, 12);
+        rlutil::locate(30, 13);
         cout << "1. UN JUGADOR" << endl;
-    /// rlutil::locate(30, 13);
+        rlutil::locate(30, 14);
         cout << "2. DOS JUGADORES" << endl;
-    /// rlutil::locate(30, 14);
+        rlutil::locate(30, 15);
+        cout << "3. MOSTRAR PUNTUACION MAXIMA" << endl;
+        rlutil::locate(30, 16);
         cout << "0. SALIR DEL JUEGO " << endl;
-    /// rlutil::locate(30, 15);
+        rlutil::locate(30, 17);
         cout << "------------------------------------------------- " << endl << endl;
-    /// rlutil::locate(30, 16);
+        rlutil::locate(30, 18);
         cout << "SELECCIONE UNA OPCION: ";
 
 
@@ -43,28 +50,29 @@ int main()
 
         system("cls"); /// al quitarlo, cuando seleccionamos un modo juego, sigue quedando el menu principal
 
-        switch(opc){
-            case 1:
-                while (partida(1));
-                break; /// al quitar el break, cuando se finaliza una partida nunca se volveria al menu principal, y se reiniciaria el juego infinitamente
-            case 2:
-                while (partida(2));
-                break;
-            case 3:
-                tirarDados(true);
-                break;
-            case 0:
-                return 0;
-                break;
+        switch(opc)
+        {
+        case 1:
+            while (partida(1));
+            break; /// al quitar el break, cuando se finaliza una partida nunca se volveria al menu principal, y se reiniciaria el juego infinitamente
+        case 2:
+            while (partida(2));
+            break;
+        case 3:
+            break;
 
-            default:
-                cout << "OPCION INVALIDA, VUELVA A INGRESAR OTRA. " << endl;
-                system("pause"); /// pausa de sistema añadida, ya que sin ella, no se llegaba a leer lo que dice
-                break;
+        case 4:
+            tirarDados(true);
+            break;
+        case 0:
+            return 0;
+            break;
 
-
+        default:
+            cout << "OPCION INVALIDA, VUELVA A INGRESAR OTRA. " << endl;
+            system("pause"); /// pausa de sistema añadida, ya que sin ella, no se llegaba a leer lo que dice
+            break;
         }
-
     }
     return 0;
 }
